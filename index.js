9b45b5d786538242eb4cbb4196030babe45f537e
Deborah Kurz <DeborahKurz6@gmail.com>
2023-10-05 09:03:43 -0600

    Basic fetch request working

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded",()=>fetchData());
 
+
 function fetchData(){
-    fetch('url')
+    const latitude = 40.71427
+    const longitude = 74.00597
+    const url = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
+    fetch(url)
     .then((resp)=>resp.json())
     .then((data)=> console.log(data))
 }
