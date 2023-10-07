b3806b424f4c33350ab1d637fd9923aff16d98e2
Deborah Kurz <DeborahKurz6@gmail.com>
2023-10-07 10:26:56 -0600

    Start Over: Initial Commit athletes db.json file

---
+++ README.md
@@ -0,0 +1,20 @@
+db.json file data links:
+//100m
+World Ranking Data: https://worldathletics.org/world-rankings/100m/women?regionType=world&page=1&rankDate=2023-10-03&limitByCountry=0
+Athlete Image: https://worldathletics.org/athletes/united-states/shacarri-richardson-14717691
+
+//200m
+World Ranking Data: https://worldathletics.org/world-rankings/200m/women?regionType=world&page=1&rankDate=2023-10-03&limitByCountry=0
+Athlete Image: https://worldathletics.org/athletes/united-states/gabrielle-thomas-14733520
+
+//400m
+World Ranking Data: https://worldathletics.org/world-rankings/400m/women?regionType=world&page=1&rankDate=2023-10-03&limitByCountry=0
+Athlete Image: https://worldathletics.org/athletes/united-states/shamier-little-14443767
+
+//800m
+World Ranking Data: https://worldathletics.org/world-rankings/800m/women?regionType=world&page=1&rankDate=2023-10-03&limitByCountry=0
+Athlete Image: https://worldathletics.org/athletes/united-states/athing-mu-14708132
+
+//1500m
+World Ranking Data: https://worldathletics.org/world-rankings/1500m/women?regionType=world&page=1&rankDate=2023-10-03&limitByCountry=0
+Athlete Image: https://worldathletics.org/athletes/united-states/cory-ann-mcgee-14316538
--- index.js
+++ index.js
@@ -1,54 +1,7 @@
-document.addEventListener("DOMContentLoaded",()=>cstCdt());
+document.addEventListener("DOMContentLoaded",()=>console.log(apiTest()))
 
-
-//BEGIN Latitude & Longitude
-function akstAkdt(){
-    //Anchorage was used for lat & lon
-    const latitude = 61.217381
-    const longitude = -149.863129
-    return fetchData(latitude,longitude); 
-}
-
-function hst(){
-    //Honolulu was used for lat & lon
-    const latitude = 21.315603
-    const longitude = -157.858093
-    return fetchData(latitude,longitude); 
-}
-
-function pstPdt(){
-    //Los Angeles was used for lat & lon
-    const latitude = 34.052235
-    const longitude = -118.243683
-    return fetchData(latitude,longitude); 
-}
-
-function mstMdt(){
-    //Denver was used for lat & lon
-    const latitude = 39.742043
-    const longitude = -104.991531
-    return fetchData(latitude,longitude); 
-}
-
-function cstCdt(){
-    //Chicago was used for lat & lon
-    const latitude = 41.85003
-    const longitude = -87.65005
-    return fetchData(latitude,longitude); 
-}
-
-function estEdt(){
-    //NYC was used for lat & lon
-    const latitude = 40.71427
-    const longitude = 74.00597
-    return fetchData(latitude,longitude);
-}
-//END Latitude & Longitude
-//BEGIN fetch request
-function fetchData(latitude, longitude){
-    const url = `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}`;
-    fetch(url)
+function apiTest(){
+    fetch('http://localhost:3000/athletes')
     .then((resp)=>resp.json())
     .then((data)=> console.log(data))
 }
-//END fetch request
